7c645d9e02fe4a737d6beb8b73804bb56a84044e
Majid Ahmed <majidahmed2003@gmail.com>
2025-04-22 12:45:21 +0530

    third commit

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 connectDB();
+
+const cors = require('cors');
+app.use(cors());
 
 const app = express();
 app.use(express.json());
